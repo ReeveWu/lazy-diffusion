3700d57a3bfb8584db6fdcdccaae3423fbb9abc9
VincentLi1216 <sunnus.tw@gmail.com>
2024-06-15 19:50:31 +0800

    Perf: refine generateDescriptions LLM

--- utils/LLM.py
+++ utils/LLM.py
@@ -10,11 +10,9 @@ from utils.Embeddings import EmbeddingModel
 
 class LLM:
     def __init__(self, prompt):
-        self.llm = Ollama(
-            model="llama3"
-        )
+        self.llm = Ollama(model="llama3")
         self.chain = prompt | self.llm | JsonOutputParser()
-        
+
     def invoke(self, query):
         return self.chain.invoke({"query": query})
 
@@ -22,13 +20,15 @@ class LLM:
 # Retrieve some reference prompt that is great for being the input of stable diffusion model
 class RetrievalWithPrompt:
     def __init__(self, mode=1):
-        self.llm = Ollama(
-            model="llama3"
-        )
+        self.llm = Ollama(model="llama3")
         self.embeddings = EmbeddingModel(mode)
-        self.db = FAISS.load_local("./utils/prompt_index", self.embeddings, allow_dangerous_deserialization=True)
+        self.db = FAISS.load_local(
+            "./utils/prompt_index",
+            self.embeddings,
+            allow_dangerous_deserialization=True,
+        )
         self.retriever = self.db.as_retriever()
-        
+
     def invoke(self, prompt):
         docs = self.retriever.invoke(prompt)
         return docs
@@ -38,14 +38,14 @@ class RetrievalWithPrompt:
 class IsGenerationalRequest(LLM):
     def __init__(self):
         super().__init__(self.get_prompt())
-        
+
     def invoke(self, query):
         return super().invoke(query)
-    
+
     @staticmethod
     def get_prompt():
         return PromptTemplate(
-                template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> \
+            template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> \
 You are an assistant who helps users generate images. \
 Your job is to determine whether the user's input is a request for you to start generating an image. \
 Users might not explicitly make this request, so here is a tip: if the user's response is not describing the requirements for an image (including composition, style, etc.), \
@@ -55,46 +55,58 @@ this usually means they likely want you to start generating the image.
 If it is, return 'yes'; otherwise, return 'no'.
 Output the answer as a JSON with a single key 'result' and no preamble or explanation. <|eot_id|><|start_header_id|>user<|end_header_id|>\
 {query}<|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
-                input_variables=["query"],
-)
+            input_variables=["query"],
+        )
+
 
 # TODO: Refine the prompt
 # The model is used to generate three more complete and detailed scene descriptions based on the user's brief description
 class GenerateDescriptions(LLM):
     def __init__(self):
         super().__init__(self.get_prompt())
-        
+
     def invoke(self, query):
         return super().invoke(query)
-    
+
     @staticmethod
     def get_prompt():
         return PromptTemplate(
-                template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> \
-Your job involves taking brief descriptions provided by users and expanding them into three more complete and detailed scene descriptions, based on the users' inputs, to enhance the effectiveness of image generation.
+            #                 template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> \
+            # Your job involves taking brief descriptions provided by users and expanding them into three more complete and detailed scene descriptions, based on the users' inputs, to enhance the effectiveness of image generation.
+            # Please add details and elements to make the scenes more vivid and specific based on this description.
+            # Each description must within 7 words. \
+            # Your answer should be a JSON with a single key 'result' and a list of three scene descriptions. and no preamble or explanation. \
+            # <|eot_id|><|start_header_id|>user<|end_header_id|>\
+            # {query}<|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+            template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> \
+    You are a specialized assistant tasked with enhancing user-provided scene descriptions for image generation. \
+    Your goal is to enrich these brief descriptions into three distinct, detailed, and visually appealing scene variations. \
+    Focus on adding creative elements and details that increase the vividness and artistic quality of each scene.
 
-Please add details and elements to make the scenes more vivid and specific based on this description. 
-Each description must within 7 words. \
+    Your response should be formatted as a JSON object containing a single key 'result'. \
+    This key should map to a list of three enhanced scene descriptions, \
+    each thoughtfully expanded to include additional elements that make the scenes more engaging and picturesque. \
+    Ensure that each description is concise yet rich in detail, aiming for a balance between creativity and clarity.
 
-Your answer should be a JSON with a single key 'result' and a list of three scene descriptions. and no preamble or explanation. \
-<|eot_id|><|start_header_id|>user<|end_header_id|>\
-{query}<|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
-                input_variables=["query"],
-)
+    <|eot_id|><|start_header_id|>user<|end_header_id|>\
+    {query}<|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+            input_variables=["query"],
+    )
+
 
 # TODO: Refine the prompt
 # The model is used to generate six different styles based on the user's sentence
 class GenerateStyle(LLM):
     def __init__(self):
         super().__init__(self.get_prompt())
-        
+
     def invoke(self, query):
         return super().invoke(query)
-    
+
     @staticmethod
     def get_prompt():
         return PromptTemplate(
-                template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> \
+            template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> \
 You are an artist whose job is to provide six different styles based on user sentences, \
 capturing the context of the sentences to allow users to choose their preferred style. \
 Your styles should be as diverse as possible, each represented by a single keyword.
@@ -102,8 +114,5 @@ Your styles should be as diverse as possible, each represented by a single keywo
 Your answer should be a JSON with a single key 'result' and a list of six different styles without any description. and no preamble or explanation. \
 <|eot_id|><|start_header_id|>user<|end_header_id|>\
 {query}<|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
-                input_variables=["query"],
-)
-
-
-        
+            input_variables=["query"],
+        )
